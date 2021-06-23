e153d9c75f10f8795d4d0d178ed2f8d144478d10
artur semikov <fowlart1988@gmail.com>
2021-06-23 13:58:01 +0300

    23.06.2021: started learn subscriptions

---
+++ src/main/java/main/controllers/stripe/connected_accs/DirectChargeController.java
@@ -0,0 +1,87 @@
+package main.controllers.stripe.connected_accs;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Account;
+import com.stripe.model.AccountLink;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.RequestOptions;
+import main.Launcher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DirectChargeController {
+
+    @GetMapping(value = "/direct-charge")
+    public ResponseEntity<String> directCharge(@RequestParam String connectedStripeAccId) throws StripeException {
+        Stripe.apiKey = Launcher.secretKey;
+        ArrayList<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+        Map<String, Object> params = new HashMap<>();
+        params.put("payment_method_types", paymentMethodTypes);
+        params.put("amount", 1000);
+        params.put("currency", "usd");
+        // With Connect, your platform can take an application fee on direct charges.
+        params.put("application_fee_amount", 123);
+        RequestOptions requestOptions = RequestOptions.builder().setStripeAccount(connectedStripeAccId).build();
+        PaymentIntent paymentIntent = PaymentIntent.create(params, requestOptions);
+        return ResponseEntity.ok().body(paymentIntent.toJson());
+    }
+
+    @GetMapping(value = "/acc-info")
+    public ResponseEntity<String> getConnectedAccInfo(@RequestParam String connectedStripeAccId) throws StripeException {
+        Stripe.apiKey = Launcher.secretKey;
+        Account account =
+                Account.retrieve(connectedStripeAccId);
+        return ResponseEntity.ok().body(account.toJson());
+    }
+
+
+    @GetMapping(value = "/create-acc")
+    public ResponseEntity<String> createAcc() throws StripeException {
+        Stripe.apiKey = Launcher.secretKey;
+
+        Map<String, Object> cardPayments = new HashMap<>();
+        cardPayments.put("requested", true);
+
+        Map<String, Object> transfers = new HashMap<>();
+        transfers.put("requested", true);
+
+        //  Map<String, Object> capabilities = new HashMap<>();
+        // capabilities.put("card_payments", cardPayments);
+        // capabilities.put("transfers", transfers);
+        //  params.put("capabilities", capabilities);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "standard");
+        params.put("country", "US");
+        params.put("email", "jenny.rosen@example.com");
+
+        Account account = Account.create(params);
+
+        Map<String, Object> params1 = new HashMap<>();
+        params1.put("account", account.getId());
+        params1.put(
+                "refresh_url",
+                "https://example.com/reauth"
+        );
+        params1.put(
+                "return_url",
+                "https://example.com/return"
+        );
+        params1.put("type", "account_onboarding");
+
+        AccountLink accountLink =
+                AccountLink.create(params1);
+
+        return ResponseEntity.ok().body(accountLink.toJson());
+    }
+
+}
--- src/main/java/main/controllers/stripe/subscribtion/Subscriptions.java
+++ src/main/java/main/controllers/stripe/subscribtion/Subscriptions.java
@@ -1,13 +1,70 @@
 package main.controllers.stripe.subscribtion;
 
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.Price;
+import com.stripe.model.Product;
+import com.stripe.net.RequestOptions;
+import com.stripe.param.PaymentIntentCreateParams;
+import main.Launcher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/stripe/subscriptions")
 public class Subscriptions {
 
+    @GetMapping(value = "/create-all-4-subscriptions")
+    public ResponseEntity<Map<String, String>> createAll4Subscriptions() throws StripeException {
+
+        RequestOptions requestOptions = RequestOptions.builder().setApiKey(Launcher.secretKey).build();
+
+        Map<String, Object> productParams = new HashMap<>();
+        productParams.put("name", "Basic");
+        Product product = Product.create(productParams, requestOptions);
+
+        Map<String, Object> recurring = new HashMap<>();
+        recurring.put("interval", "month");
+        Map<String, Object> priceParams = new HashMap<>();
+        priceParams.put("unit_amount", 2000);
+        priceParams.put("currency", "USD");
+        priceParams.put("recurring", recurring);
+        priceParams.put("product", product.getId());
+        priceParams.put("lookup_key", "sample_basic");
+
+        Price price = Price.create(priceParams, requestOptions);
+        Map<String, String> customResponse = new HashMap<>();
+
+        customResponse.put("product", product.toJson());
+        customResponse.put("price", price.toJson());
+
+        return ResponseEntity.ok().body(customResponse);
+    }
+
+    @GetMapping(value = "/create-payment-intent")
+    public ResponseEntity<Map<String,String>> createPaymentIntent() throws StripeException {
+
+        RequestOptions requestOptions = RequestOptions.builder().setApiKey(Launcher.secretKey).build();
+
+        PaymentIntentCreateParams paymentIntentCreateParams = PaymentIntentCreateParams
+                .builder()
+                .setAmount(1L)
+                .setCurrency("usd")
+                .build();
+
+        PaymentIntent paymentIntent = PaymentIntent.create(paymentIntentCreateParams, requestOptions);
+
+        Map<String,String> customResponse = new HashMap<>();
+        customResponse.put("client secret 4 PaymentIntent",paymentIntent.getClientSecret());
+
+        return ResponseEntity.ok().body(customResponse);
+    }
 
 
 }
